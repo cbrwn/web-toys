710c122f3dcb635e2b73293545091ac4a43d06cc
Cameron Brown <c@meronbrown.com>
2023-03-08 01:03:51 +1100

    add cool new font

--- app.vue
+++ app.vue
@@ -5,7 +5,10 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Itim&display=swap');
+
 body {
   background-color: rgb(218, 221, 230);
+  font-family: 'Itim', cursive, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
 }
 </style>
