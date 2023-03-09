153545234437059f1fabea7081e49f4c2796cc8f
Cameron Brown <c@meronbrown.com>
2023-03-10 01:28:06 +1100

    add adding, removing, and editing of items

--- components/wheel/Item.vue
+++ components/wheel/Item.vue
@@ -1,9 +1,13 @@
 <template>
 	<div class="flex flex-row min-w-full bg-slate-200 hover:bg-slate-300 p-2 rounded-lg">
 		<div class="flex items-center justify-center flex-grow">
-			<slot />
+			<span v-if="!editMode" v-on:dblclick="editClicked">{{ itemName }}</span>
+			<input v-if="editMode" ref="editBox" :value="itemName" v-on:keypress="onEditPress" size="10">
 		</div>
-		<div class="select-none text-2xl">
+		<div class="select-none text-2xl flex gap-2">
+			<span @click="deleteClicked" class="cursor-pointer" v-if="showDelete">❌</span>
+			<span @click="editClicked" class="cursor-pointer" v-if="!editMode">️✍️</span>
+			<span @click="editClicked" class="cursor-pointer" v-if="editMode">️✅</span>
 			<span @click="moveClicked" class="cursor-pointer">{{ moveIcon }}️</span>
 		</div>
 	</div>
@@ -11,10 +15,41 @@
 
 <script>
 export default {
-	props: ['onMove', 'moveIcon'],
+	props: ['onMove', 'itemName', 'moveIcon', 'showDelete'],
+	data() {
+		return {
+			editMode: false
+		};
+	},
+	watch: {
+		itemName: function () {
+			console.log(this.itemName);
+			if (this.itemName == 'new item' && !this.editMode) {
+				console.log('clicky');
+				this.editClicked();
+			}
+		}
+	},
 	methods: {
 		moveClicked() {
 			this.$emit("onMove");
+		},
+		deleteClicked() {
+			this.$emit("onDelete");
+		},
+		editClicked() {
+			if (this.editMode) {
+				this.$emit("itemEdited", this.$refs.editBox.value);
+			}
+
+			this.editMode = !this.editMode;
+
+			if (this.editMode) {
+				setTimeout(() => this.$refs.editBox.focus(), 1);
+			}
+		},
+		onEditPress(event) {
+			if (this.editMode && event.key == 'Enter') this.editClicked();
 		}
 	}
 }
--- components/wheel/ItemEditor.vue
+++ components/wheel/ItemEditor.vue
@@ -3,16 +3,16 @@
 		<h1 class="text-4xl">items</h1>
 		<div class="flex flex-row gap-2 h-96 overflow-auto">
 			<div class="flex flex-col flex-grow items-center justify-start w-1/2 gap-2">
-				<h2 class="text-2xl">active</h2>
-				<WheelItem v-for="(item, index) in items" @on-move="moveToBench(item)" :moveIcon="bench.length > 0 ? (items.length>1 ? '➡️' : '') : '🫥'">
-					{{ item }}
-				</WheelItem>
+				<h2 class="text-2xl">active <span class="cursor-pointer" v-on:click="addClicked">➕</span></h2>
+				<WheelItem v-for="(item, index) in items" @on-move="moveToBench(item)"
+					@item-edited="(i) => onItemEdited(index, i)" :itemName="item"
+					:moveIcon="bench.length > 0 ? (items.length > 1 ? '➡️' : '') : '🫥'" />
 			</div>
-			<div class="flex flex-col flex-grow items-center justify-start w-1/2 gap-2" v-if="bench.length>0">
+			<div class="flex flex-col flex-grow items-center justify-start w-1/2 gap-2" v-if="bench.length > 0">
 				<h2 class="text-2xl">bench</h2>
-				<WheelItem v-for="(item, index) in bench" @on-move="moveToActive(item)" moveIcon="⬅️">
-					{{ item }}
-				</WheelItem>
+				<WheelItem v-for="(item, index) in bench" @on-move="moveToActive(item)"
+					@item-edited="(i) => onBenchEdited(index, i)" @on-delete="deleteFromBench(index)" :itemName="item"
+					moveIcon="⬅️" showDelete="true" />
 			</div>
 		</div>
 	</div>
@@ -31,6 +31,27 @@ export default {
 			let newItems = this.items.concat([item]);
 			let newBench = this.bench.filter((i) => i != item);
 			this.$emit("itemsChanged", { items: newItems, bench: newBench });
+		},
+
+		addClicked() {
+			let newItems = ['new item'].concat(this.items);
+			this.$emit("itemsChanged", { items: newItems, bench: this.bench });
+		},
+
+		onItemEdited(index, newValue) {
+			let newItems = [...this.items];
+			newItems[index] = newValue;
+			this.$emit("itemsChanged", { items: newItems, bench: this.bench });
+		},
+		onBenchEdited(index, newValue) {
+			let newBench = [...this.bench];
+			newBench[index] = newValue;
+			this.$emit("itemsChanged", { items: this.items, bench: newBench });
+		},
+		deleteFromBench(index) {
+			let newBench = [...this.bench];
+			newBench.splice(index, 1);
+			this.$emit("itemsChanged", { items: this.items, bench: newBench });
 		}
 	}
 }
