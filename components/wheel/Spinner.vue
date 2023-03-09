d545e78e868184ec8a21708aaaaeeea0a3aee79e
Cameron Brown <c@meronbrown.com>
2023-03-09 20:14:44 +1100

    wheel now spins nicely, result is emitted to page and shown

--- components/wheel/Spinner.vue
+++ components/wheel/Spinner.vue
@@ -1,51 +1,87 @@
 <template>
-    <canvas class="w-min transition-transform" v-on:click="newRotate" :style="rotateStyle" ref="wheel" width="400" height="400" />
+    <canvas 
+        class="w-min transition-transform" 
+        :class="{['cursor-pointer']: !spinning}"
+        @transitionend="onSpinEnd"
+        @webkitTransitionend="onSpinEnd"
+        v-on:click="newRotate" 
+        :style="rotateStyle" 
+        ref="wheel" 
+        width="400" height="400" 
+    />
 </template>
 
 <script>
 export default {
-    props: ['items'],
+    props: {
+        items: Array,
+        spinCount: { // number of times to spin before landing on the chosen item
+            type: Number,
+            default: 6
+        }
+    },
     mounted: function () {
-        this.drawCanvas();
+        document.fonts.ready.then(() => this.drawCanvas());
     },
     data() {
         let newRotateDeg = Math.random()*360;
         return {
-            rotateEndDeg: newRotateDeg
+            rotateEndDeg: newRotateDeg,
+            spinning: false,
+            selectedItem: 0
         }
     },
     computed: {
         rotateStyle: function () {
+            let timingFunc = 'cubic-bezier(.35,-0.1,.02,1)';
             return {
                 '-webkit-transform': `rotateZ(${this.rotateEndDeg}deg)`,
                 transform: `rotateZ(${this.rotateEndDeg}deg)`,
                 '-webkit-transition-duration': `${this.rotateDuration}s`,
                 'transition-duration': `${this.rotateDuration}s`,
-                '-webkit-transition-timing-function:': this.timingFun,
-                'transition-timing-function': this.timingFun
+                '-webkit-transition-timing-function:': timingFunc,
+                'transition-timing-function': timingFunc
             }
+        },
+        rotateDuration: function() {
+            return this.spinning ? 8 : 0;
+        },
+        canSpin: function() {
+            return !this.spinning;
         }
     },
     methods: {
         newRotate: function(event) {
-            let newRotateDeg = Math.random()*360;
-            this.rotateEndDeg = newRotateDeg;
+            if(!this.canSpin) return;
+
+            let itemIndex = Math.floor(Math.random()*this.items.length);
+            this.selectedItem = itemIndex;
+            let itemAngle = this.getAngleForItem(itemIndex);
+
+            this.rotateEndDeg = (360*this.spinCount) + itemAngle;
+            this.spinning = true;
+
+            this.$emit('spinStart');
+        },
+        onSpinEnd: function() {
+            this.spinning = false;
+            // get rid of the extra spins, ready for next spin
+            this.rotateEndDeg %= 360;
+
+            this.$emit('spinEnd', {item: this.items[this.selectedItem], itemIndex: this.selectedItem});
         },
         drawCanvas: function () {
             const canvas = this.$refs.wheel;
-            console.log(canvas.width);
             if (canvas.getContext) {
                 const ctx = canvas.getContext("2d");
                 const radius = canvas.width / 2;
                 const sliceAngle = (Math.PI * 2) / (this.items.length);
-                console.log(this.items.length);
-                console.log(sliceAngle);
 
 
                 ctx.font = `24px Itim`
 
                 for (let i = 0; i < this.items.length; ++i) {
-                    let startAngle = sliceAngle * i;
+                    let startAngle = sliceAngle * i - (Math.PI/2);
 
                     ctx.fillStyle = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
                     ctx.beginPath();
@@ -65,6 +101,10 @@ export default {
                     ctx.restore()
                 }
             }
+        },
+        getAngleForItem: function(itemIndex) {
+            const angleBetweenItems = 360/this.items.length;
+            return 360 - ((angleBetweenItems*itemIndex) + (Math.random() * angleBetweenItems));
         }
     }
 }
--- pages/the-wheel.vue
+++ pages/the-wheel.vue
@@ -4,7 +4,16 @@
       <Header title="the wheel!">
       </Header>
       <ContentContainer>
-        <WheelSpinner :items="items" />
+        <div class="transition-all duration-300 select-none" @transitionend="resultSubtextFinished"
+          style="margin-top: -20px; margin-bottom: 20px;" :style="`transform: scale(${this.resultState >= 1 ? '1.0' : '0.0'})`">
+          the wheel has chosen...
+          <p class="text-4xl transition-all duration-500" :style="`transform: scale(${this.resultState >= 2 ? '1.0' : '0.0'})`" @click="hideResult">
+            <span class="animate-spin inline-block">🌟</span>
+            {{ resultToShow }}!!
+            <span class="animate-spin inline-block">🌟</span>
+          </p>
+        </div>
+        <WheelSpinner :items="items" @spin-start="onSpinStart" @spin-end="onSpinFinish" style="margin-bottom: 40px"/>
       </ContentContainer>
     </div>
   </div>
@@ -16,25 +25,44 @@ export default {
   created() { document.title = 'the wheel.'; },
   data() {
     let initItems = ['cam', 'matt', 'darcy 1', 'darcy 2',
-          'jeff',
-          'bob',
-          'jason',
-          'aaron',
-          'ben',
-          'greg',
-          'henry',
-          'jake',
-          'larry',
-          'tom',
-          'fred',
-         ];
+      'jeff',
+      'bob',
+      'jason',
+      'aaron',
+      'ben',
+      'greg',
+      'henry',
+      'jake',
+      'larry',
+      'tom',
+      'fred',
+    ];
     if (process.client) {
       if (localStorage.wheelItems)
         initItems = JSON.parse(localStorage.wheelItems);
     }
     return {
-      items: initItems
+      items: initItems,
+      resultState: 0,
+      resultToShow: null
     };
+  },
+  methods: {
+    hideResult() {
+      this.resultState = 0;
+    },
+    onSpinStart() {
+      this.hideResult();
+    },
+    onSpinFinish(selectedItem) {
+      this.resultState = 1;
+      this.resultToShow = selectedItem.item;
+    },
+    resultSubtextFinished() {
+      if (this.resultState < 1) return;
+
+      setTimeout(() => this.resultState++, 300);
+    }
   }
 }
 </script>
