14ef14864a71fbcadc4ec013ae9a6433ffd3c72d
Cameron Brown <c@meronbrown.com>
2023-03-12 11:33:02 +1100

    separate scroll boxes for active/bench, and retain header when scrolling

--- components/wheel/ItemEditor.vue
+++ components/wheel/ItemEditor.vue
@@ -1,29 +1,34 @@
 <template>
 	<div class="w-96">
 		<div class="flex justify-center items-center">
-		<h1 class="text-4xl flex-grow">items</h1>
-		<span class="flex-shrink text-2xl select-none">
-			<span v-if="showCopySuccess">
-				✅
+			<h1 class="text-4xl flex-grow">items</h1>
+			<span class="flex-shrink text-2xl select-none">
+				<span v-if="showCopySuccess">
+					✅
+				</span>
+				<span class="cursor-pointer" v-on:click="copyItemsClicked" v-else>📋</span>
+				<span class="text-lg absolute transition-transform"
+					:style="`transform: translate(-75%, ${this.showCopySuccess ? '-100%' : '0%'}) scale(${this.showCopySuccess ? '1.0' : '0.0'});`">
+					copied url!
+				</span>
 			</span>
-			<span class="cursor-pointer" v-on:click="copyItemsClicked" v-else>📋</span>
-			<span class="text-lg absolute transition-transform" :style="`transform: translate(-75%, ${this.showCopySuccess ? '-100%' : '0%'}) scale(${this.showCopySuccess ? '1.0' : '0.0'});`">
-				copied url!
-			</span>
-		</span>
 		</div>
-		<div class="flex flex-row gap-2 h-96 overflow-auto">
+		<div class="flex flex-row gap-2 h-96">
 			<div class="flex flex-col flex-grow items-center justify-start w-1/2 gap-2">
 				<h2 class="text-2xl">active <span class="cursor-pointer" v-on:click="addClicked">➕</span></h2>
-				<WheelItem v-for="(item, index) in items" @on-move="moveToBench(item)"
-					@item-edited="(i) => onItemEdited(index, i)" :itemName="item"
-					:moveIcon="bench.length > 0 ? (items.length > 1 ? '➡️' : '') : '🫥'" :enabled="enabled" />
+				<div class="overflow-auto w-full flex flex-col gap-2">
+					<WheelItem v-for="(item, index) in items" @on-move="moveToBench(item)"
+						@item-edited="(i) => onItemEdited(index, i)" :itemName="item"
+						:moveIcon="bench.length > 0 ? (items.length > 1 ? '➡️' : '') : '🫥'" :enabled="enabled" />
+				</div>
 			</div>
 			<div class="flex flex-col flex-grow items-center justify-start w-1/2 gap-2" v-if="bench.length > 0">
 				<h2 class="text-2xl">bench</h2>
-				<WheelItem v-for="(item, index) in bench" @on-move="moveToActive(item)"
-					@item-edited="(i) => onBenchEdited(index, i)" @on-delete="deleteFromBench(index)" :itemName="item"
-					moveIcon="⬅️" showDelete="true" :enabled="enabled" />
+				<div class="overflow-auto w-full flex flex-gol gap-2">
+					<WheelItem v-for="(item, index) in bench" @on-move="moveToActive(item)"
+						@item-edited="(i) => onBenchEdited(index, i)" @on-delete="deleteFromBench(index)" :itemName="item"
+						moveIcon="⬅️" showDelete="true" :enabled="enabled" />
+				</div>
 			</div>
 		</div>
 	</div>
@@ -39,38 +44,38 @@ export default {
 	},
 	methods: {
 		moveToBench(item) {
-			if(!this.enabled) return;
+			if (!this.enabled) return;
 			let newItems = this.items.filter((i) => i != item);
 			let newBench = this.bench.concat([item]);
 			this.$emit("itemsChanged", { items: newItems, bench: newBench });
 		},
 		moveToActive(item) {
-			if(!this.enabled) return;
+			if (!this.enabled) return;
 			let newItems = this.items.concat([item]);
 			let newBench = this.bench.filter((i) => i != item);
 			this.$emit("itemsChanged", { items: newItems, bench: newBench });
 		},
 
 		addClicked() {
-			if(!this.enabled) return;
+			if (!this.enabled) return;
 			let newItems = ['new item'].concat(this.items);
 			this.$emit("itemsChanged", { items: newItems, bench: this.bench });
 		},
 
 		onItemEdited(index, newValue) {
-			if(!this.enabled) return;
+			if (!this.enabled) return;
 			let newItems = [...this.items];
 			newItems[index] = newValue;
 			this.$emit("itemsChanged", { items: newItems, bench: this.bench });
 		},
 		onBenchEdited(index, newValue) {
-			if(!this.enabled) return;
+			if (!this.enabled) return;
 			let newBench = [...this.bench];
 			newBench[index] = newValue;
 			this.$emit("itemsChanged", { items: this.items, bench: newBench });
 		},
 		deleteFromBench(index) {
-			if(!this.enabled) return;
+			if (!this.enabled) return;
 			let newBench = [...this.bench];
 			newBench.splice(index, 1);
 			this.$emit("itemsChanged", { items: this.items, bench: newBench });
@@ -82,7 +87,7 @@ export default {
 			navigator.clipboard.writeText(currentLocation + params);
 
 			this.showCopySuccess = true;
-			setTimeout(() => this.showCopySuccess=false, 3000);
+			setTimeout(() => this.showCopySuccess = false, 3000);
 		}
 	}
 }
